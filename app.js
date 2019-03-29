738f5066fd6776cb0810518eb5310f9d89879f5b
diewland <diewland@gmail.com>
2019-03-29 10:02:10 +0700

    change menu sep

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let menus = [
   [ 'Home', 'index.html' ],
   [ 'Face', 'face.html' ],
 ];
-$('.menu').html(menus.map((m) => `<a href='${m[1]}'>${m[0]}</a>`).join(' : '));
+$('.menu').html(menus.map((m) => `<a href='${m[1]}'>${m[0]}</a>`).join(' | '));
 
 // prevent link cache
 $('a').each((i, o) => {
