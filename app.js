2be70fb467385b8b7a7b2293fece7c654cf86370
diewland <diewland@gmail.com>
2019-03-29 09:45:11 +0700

    test shorthand js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ let menus = [
   [ 'Home', 'index.html' ],
   [ 'Face', 'face.html' ],
 ];
-// $('.menu').html(menus.map((m) => `<a href='${m[1]}'>${m[0]}</a>`).join(' | '));
-   $('.menu').html(menus.map((m) => { return `<a href='${m[1]}'>${m[0]}</a>` }).join(' | '));
+$('.menu').html(menus.map((m) => `<a href='${m[1]}'>${m[0]}</a>`).join(' | '));
+// $('.menu').html(menus.map((m) => { return `<a href='${m[1]}'>${m[0]}</a>` }).join(' | '));
 
 // prevent link cache
 $('a').each((i, o) => {
