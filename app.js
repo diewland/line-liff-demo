7d9a37097b7a2766bd3d028515934c1a9a68115c
diewland <diewland@gmail.com>
2019-03-29 09:37:52 +0700

    revise menu script

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ let menus = [
   [ 'Home', 'index.html' ],
   [ 'Face', 'face.html' ],
 ];
-$('.menu').html(menus.map((m) => `<a href='${m[1]}'>${m[0]}</a>`).join(' | '));
+// $('.menu').html(menus.map((m) => `<a href='${m[1]}'>${m[0]}</a>`).join(' | '));
+let menu_html = menus.map(function(m){
+  return `<a href='${m[1]}'>${m[0]}</a>`;
+}).join(' | ');
+$('.menu').html(menu_html);
 
 // prevent link cache
 $('a').each((i, o) => {
